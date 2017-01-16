215f29f47558a87e9cace1a25a063c38855efd3a
jacrylodai-woshi <jacrylodai@sina.com>
2017-01-16 19:58:54 +0800

    *定位到我的位置，无法显示位置圆点

--- src/com/ldp/android/baidumaptestnew/activity/ComplexMapActivity.java
+++ src/com/ldp/android/baidumaptestnew/activity/ComplexMapActivity.java
@@ -1,24 +1,41 @@
 package com.ldp.android.baidumaptestnew.activity;
 
+import org.apache.http.util.LangUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.ldp.android.baidumaptestnew.R;
 
 public class ComplexMapActivity extends Activity{
+	
+	private static final String TAG = ComplexMapActivity.class.getSimpleName();
 	
 	private MapView mMVCity;
 	
 	private BaiduMap mBaiduMap;
 	
 	private boolean mShowCityStreet;
+	
+	private LocationClient mLocationClient;
+	
+	private MyLocationListener mMyLocationListener;
+	
+	private boolean firstLocate = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +47,27 @@ public class ComplexMapActivity extends Activity{
         
         mMVCity = (MapView) findViewById(R.id.mv_city);
         
-        initial();
+        initialMap();
+        
+        initialMyLocation();
 	}
 	
-	private void initial() {
+	private void initialMyLocation() {
+
+		
+		mLocationClient = new LocationClient(this);
+		mMyLocationListener = new MyLocationListener();
+		
+		LocationClientOption option = new LocationClientOption();
+		option.setOpenGps(true);
+		option.setCoorType("bd09ll");
+		option.setScanSpan(5*1000);
+		
+		mLocationClient.setLocOption(option);
+		
+	}
+
+	private void initialMap() {
 
 		mBaiduMap = mMVCity.getMap();
 		mShowCityStreet = false;
@@ -44,20 +78,47 @@ public class ComplexMapActivity extends Activity{
 
 	@Override
 	protected void onDestroy() {
-		super.onDestroy();
+
 		mMVCity.onDestroy();
+		super.onDestroy();
 	}
 	
 	@Override
 	protected void onResume() {
+		
+		Log.i(TAG, "onResume");
+		
 		super.onResume();
 		mMVCity.onResume();
+		mBaiduMap.setMyLocationEnabled(true);
+		mLocationClient.registerLocationListener(mMyLocationListener);
+		mLocationClient.start();
 	}
 	
 	@Override
 	protected void onPause() {
-		super.onPause();
+
+		Log.i(TAG, "onPause");
+		
+		mLocationClient.stop();
+		mLocationClient.unRegisterLocationListener(mMyLocationListener);
+		mBaiduMap.setMyLocationEnabled(false);
 		mMVCity.onPause();
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStart() {
+		
+		Log.i(TAG, "onStart");
+		super.onStart();
+	}
+	
+	@Override
+	protected void onStop() {
+		
+		Log.i(TAG, "onStop");
+		super.onStop();
 	}
 	
 	@Override
@@ -95,6 +156,41 @@ public class ComplexMapActivity extends Activity{
 
 		default:
 			return super.onOptionsItemSelected(item);
+		}
+		
+	}
+	
+	private void locationToPosition(LatLng latLng){
+		
+		MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(latLng);
+		mBaiduMap.animateMapStatus(update);
+	}
+	
+	private class MyLocationListener implements BDLocationListener{
+
+		@Override
+		public void onReceiveLocation(BDLocation bdLocation) {
+
+			if(firstLocate){
+				firstLocate = false;
+				LatLng latLng = new LatLng(bdLocation.getLatitude(),bdLocation.getLongitude());
+				locationToPosition(latLng);
+			}
+			
+			MyLocationData myLocationData = 
+					new MyLocationData.Builder()
+						.accuracy(bdLocation.getRadius())
+						.latitude(bdLocation.getAltitude())
+						.longitude(bdLocation.getLongitude())
+						.build();
+			
+			mBaiduMap.setMyLocationData(myLocationData);
+		}
+
+		@Override
+		public void onReceivePoi(BDLocation arg0) {
+			// TODO Auto-generated method stub
+			
 		}
 		
 	}
